1c85e3921f85e06a620958ecbc4be6665028e14d
sizer <shinse.tanaka@gmail.com>
2019-04-08 22:51:09 +0900

    ng add apollo-angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { IssueListComponent } from './issueList.component'
+import { IssueListComponent } from './issueList.component';
+import { GraphQLModule } from './graphql.module';
+import { HttpClientModule } from '@angular/common/http'
 
 @NgModule({
   declarations: [
@@ -10,7 +12,9 @@ import { IssueListComponent } from './issueList.component'
     IssueListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    GraphQLModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
