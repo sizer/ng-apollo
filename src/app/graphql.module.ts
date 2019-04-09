aa9e742fc949c5246b37748861f916a2a6d040f4
sizer <shinse.tanaka@gmail.com>
2019-04-09 09:07:07 +0900

    add fetching query

--- src/app/graphql.module.ts
+++ src/app/graphql.module.ts
@@ -1,12 +1,25 @@
-import {NgModule} from '@angular/core';
-import {ApolloModule, APOLLO_OPTIONS} from 'apollo-angular';
-import {HttpLinkModule, HttpLink} from 'apollo-angular-link-http';
-import {InMemoryCache} from 'apollo-cache-inmemory';
+import { NgModule } from '@angular/core';
+import { HttpHeaders } from '@angular/common/http';
+import { setContext } from 'apollo-link-context';
+import { ApolloModule, APOLLO_OPTIONS } from 'apollo-angular';
+import { HttpLinkModule, HttpLink } from 'apollo-angular-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
 
-const uri = ''; // <-- add the URL of the GraphQL server here
+import { environment } from '../environments/environment'
+
+const uri = 'https://api.github.com/graphql'; // <-- add the URL of the GraphQL server here
 export function createApollo(httpLink: HttpLink) {
+  const http = httpLink.create({uri});
+  const token = environment.ghToken
+
+  const auth = setContext((_, { headers }) => {
+    return {
+      headers: new HttpHeaders().set('Authorization', `Bearer ${token}`)
+    };
+  });
+
   return {
-    link: httpLink.create({uri}),
+    link: auth.concat(http),
     cache: new InMemoryCache(),
   };
 }
--- src/app/issueList.component.ts
+++ src/app/issueList.component.ts
@@ -1,9 +1,49 @@
 import { Component } from '@angular/core';
+import { Apollo } from 'apollo-angular';
+import gql from 'graphql-tag';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators'
+
+import { environment } from '../environments/environment'
+
+const query = gql`
+  query($owner: String!, $name: String!) {
+    repository(owner: $owner, name: $name) {
+      issues(first: 100) {
+        edges {
+          node {
+            title
+          }
+        }
+      }
+    }
+  }
+`;
+
+type Edge = {
+  node: any
+}
+
+type Issue = {
+  edges: Edge[]
+}
+
+type Repository = {
+  issues: Issue
+}
+
+type Response = {
+  repository: Repository,
+};
 
 const template = `
-  <ol>
-    <li>Issue#1</li>
-    <li>Issue#2</li>
+  <ol id="issue-list">
+    <label for="issue-list">
+      Issue List
+    </label>
+    <li *ngFor="let issue of issues | async">
+      {{issue.node.title}}
+    </li>
   </ol>
 `;
 
@@ -12,7 +52,23 @@ const template = `
   template
 })
 class IssueListComponent {
-  title = 'ng-apollo';
+  issues: Observable<any>;
+
+  constructor(private apollo: Apollo) {}
+
+  ngOnInit() {
+
+    const owner = environment.repositoryPath.split("/")[0];
+    const name = environment.repositoryPath.split("/")[1];
+
+    this.issues = this.apollo
+      .watchQuery<Response>({
+        query,
+        variables: { owner, name }
+      })
+      .valueChanges
+      .pipe(map(result => result.data && result.data.repository.issues.edges));
+  }
 }
 
 export { IssueListComponent };
